b23424345bcb6508ca9c5b5caf0bd0950adfa583
@kidpollo <kidpollo@gmail.com>
2017-01-13 15:21:42 -0800

    work on for any site

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,8 +3,9 @@ class WelcomeController < ApplicationController
   end
 
   def auth
-    @callback_url = "http://localhost:5000/welcome/callback"
-    @consumer = OAuth::Consumer.new("lGF3bQY755QbAqSkwkXS", "52axpF49nSnGvSdefwKJVqKPHPaLUxRbL0bhSLjs", site: "https://young4.desk.local")
+    site = params[:site]
+    @callback_url = "https://desk-ca-testing.herokuapp.com/welcome/callback"
+    @consumer = OAuth::Consumer.new("3Q04dJYZff3GPBqT5h6o", "J6lEUg5bpElLTF9UruTY1nFeJkHDyMt6f9oxpUIa", site: site)
     @request_token = @consumer.get_request_token(oauth_callback: @callback_url)
 
     session[:token] = @request_token.token
@@ -14,9 +15,12 @@ class WelcomeController < ApplicationController
   end
 
   def callback
+    referer = request.headers["Referer"]
+    site = "https://#{URI.parse(referer).host}"
+    
     hash = { oauth_token: session[:token], oauth_token_secret: session[:token_secret]}
     @consumer = session[:consumer]
-    @consumer = OAuth::Consumer.new("lGF3bQY755QbAqSkwkXS", "52axpF49nSnGvSdefwKJVqKPHPaLUxRbL0bhSLjs", site: "https://young4.desk.local")
+    @consumer = OAuth::Consumer.new("3Q04dJYZff3GPBqT5h6o", "J6lEUg5bpElLTF9UruTY1nFeJkHDyMt6f9oxpUIa", site: site)
     @request_token  = OAuth::RequestToken.from_hash(@consumer, hash)
     @access_token = @request_token.get_access_token(oauth_verifier: params["oauth_verifier"])
     @photos = @access_token.get('/api/v2/users')
