3db5db4cd100e149b3ebc7df9e9b0dceebfa15ff
@kidpollo <kidpollo@gmail.com>
2017-01-13 15:44:56 -0800

    show something

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -17,12 +17,12 @@ class WelcomeController < ApplicationController
   def callback
     referer = request.headers["Referer"]
     site = "https://#{URI.parse(referer).host}"
-    
+
     hash = { oauth_token: session[:token], oauth_token_secret: session[:token_secret]}
     @consumer = session[:consumer]
     @consumer = OAuth::Consumer.new("3Q04dJYZff3GPBqT5h6o", "J6lEUg5bpElLTF9UruTY1nFeJkHDyMt6f9oxpUIa", site: site)
     @request_token  = OAuth::RequestToken.from_hash(@consumer, hash)
     @access_token = @request_token.get_access_token(oauth_verifier: params["oauth_verifier"])
-    @photos = @access_token.get('/api/v2/users')
+    @cases = @access_token.get('/api/v2/users')
   end
 end
