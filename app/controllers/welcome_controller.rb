8ffd625645c9be29b87474beaafd3cf610b7f836
@kidpollo <kidpollo@gmail.com>
2017-02-07 16:41:38 -0800

    pod1

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,7 +5,7 @@ class WelcomeController < ApplicationController
   def auth
     site = params[:site]
     @callback_url = "https://desk-ca-testing.herokuapp.com/welcome/callback"
-    @consumer = OAuth::Consumer.new("bMbMqj0JgMS5gM9dxREE", "BVldWVmzSx3OPvO26qIFIvZefp95WYGpQ8sr24dU", site: site)
+    @consumer = OAuth::Consumer.new("qkce0wYrz69RMEQkwKVT", "VDEhLORo23v3UCcLKpQjRE3VbMn1dGSheb5vdO7t", site: site)
     @request_token = @consumer.get_request_token(oauth_callback: @callback_url)
 
     session[:token] = @request_token.token
@@ -20,7 +20,7 @@ class WelcomeController < ApplicationController
 
     hash = { oauth_token: session[:token], oauth_token_secret: session[:token_secret]}
     @consumer = session[:consumer]
-    @consumer = OAuth::Consumer.new("bMbMqj0JgMS5gM9dxREE", "BVldWVmzSx3OPvO26qIFIvZefp95WYGpQ8sr24dU", site: site)
+    @consumer = OAuth::Consumer.new("qkce0wYrz69RMEQkwKVT", "VDEhLORo23v3UCcLKpQjRE3VbMn1dGSheb5vdO7t", site: site)
     @request_token  = OAuth::RequestToken.from_hash(@consumer, hash)
     @access_token = @request_token.get_access_token(oauth_verifier: params["oauth_verifier"])
     @cases = @access_token.get('/api/v2/cases?status=pending')
